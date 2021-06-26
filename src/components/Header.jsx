f013f92ea7d10281c44686c391d6658277cf0107
Nur <nur@gmail.com>
2021-06-26 12:59:07 +0600

    breakdown into components

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return <header className="header">
+        <img src="https://imprexismedia.com/wp-content/uploads/2020/03/php-1-logo-png-transparent.png" width='115px'/>
+    </header>
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className="content">
+        <div>
+            <img src="https://avatars.mds.yandex.net/get-zen_doc/3901320/pub_5fb625596bd43436fc0bc045_5fb62781541585566c4a1217/scale_1200"/>          
+        </div>
+
+        <div>
+            ava + desc
+        </div>
+
+        <div>
+            My posts
+            <div>
+                new posts
+            </div>
+        <div>
+            post 1
+        </div>
+        
+        <div>
+            post 2
+        </div>
+        </div>
+  </div>
+};
+
+export default Profile;
